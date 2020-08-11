87eb91109d2e81bbc3b08552271602d2ff4ecb1a
ellen-li-rft <68639724+ellen-li-rft@users.noreply.github.com>
2020-08-11 22:49:03 +0800

    Update create_some_files.sh

--- tutorials/basic/task-outputs-to-inputs/create_some_files.sh
+++ tutorials/basic/task-outputs-to-inputs/create_some_files.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir some-files
+mkdir some-files1
 echo "file7" > some-files/file7
 echo "file8" > some-files/file8
 echo "file5" > some-files/file5
 echo "file6" > some-files/file6
 
-ls some-files/*
+ls 
